27c8635c8c1228e6a9be18491576477bc0903c2d
Ben <benmahoney10@gmail.com>
2025-05-13 11:25:04 +1200

    Minor changes

--- online-poker/src/components/pages/LandingPage.tsx
+++ online-poker/src/components/pages/LandingPage.tsx
@@ -45,7 +45,7 @@ const LandingPage = () => {
     <section className="landing-page">
       <div className="landing-content">
         <h1>Poker</h1>
-        <h2>Welcome {user?.isAnonymous ? "Anonymous" : user?.displayName || "User"}</h2>
+        <h3>Welcome {user?.isAnonymous ? "Anonymous" : user?.displayName || "User"}</h3>
         <div className="landing-description">
           <p>
             This is a poker game application where you can play poker with your friends.

4de64deb0de90923c10b9892dc8b6a3a2fac8e18
Ben <benmahoney10@gmail.com>
2025-06-16 10:24:38 +1200

    Added toast sucess and fail feedback to user

--- online-poker/src/hooks/useCreateGame.ts
+++ online-poker/src/hooks/useCreateGame.ts
@@ -2,11 +2,14 @@ import { useFirestoreFunctions } from "../hooks/useFirestoreFunctions";
 import { deckShuffle } from "../utils/deckShuffle";
 import { useAuth } from "../context/FirebaseAuthContext";
 import { useGameDetails } from "../context/GameContext";
+import useAsyncFunction from "./useAsyncFunction";
 
 export const useCreateGame = () => {
   const { createGameDoc, setGameDoc } = useFirestoreFunctions();
   const { user } = useAuth();
   const { setGameID } = useGameDetails();
+  const gameAsync = useAsyncFunction<any>();
+
 
   const processGameCreate = async (ownerUID: string) => {
     const deck = deckShuffle();
@@ -21,7 +24,17 @@ export const useCreateGame = () => {
       playerCount: 1,
     };
 
-    const docRef = await createGameDoc(gameFields);
+    const docRef = await gameAsync.execute(
+      async () => {
+        return await createGameDoc(gameFields);
+      },
+      {
+        loadingMessage: "Creating game...",
+        successMessage: "Game created successfully!",
+        errorMessage: "Failed to create game",
+      }
+    );
+
     const gameId = docRef.id;
     const memberFields = {
       displayName: user?.displayName || "Anonymous Player",

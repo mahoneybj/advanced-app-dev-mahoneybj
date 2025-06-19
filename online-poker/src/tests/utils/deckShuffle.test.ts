7330eba11f979696ee29770a4b787296d89bd01e
Ben <benmahoney10@gmail.com>
2025-06-19 14:01:34 +1200

    Created deck shuffle tests

---
+++ online-poker/src/tests/utils/deckShuffle.test.ts
@@ -0,0 +1,25 @@
+import { deckShuffle } from "../../utils/deckShuffle";
+
+describe("deckShuffle", () => {
+  test("returns a shuffled deck of cards", () => {
+    const deck = deckShuffle();
+    expect(deck).toHaveLength(52);
+    expect(deck).toEqual(
+      expect.arrayContaining([
+        expect.stringMatching(/^[2-9TJQKA][CDHS]$/),
+      ]),
+    );
+  });
+
+  test("shuffles the deck randomly", () => {
+    const deck1 = deckShuffle();
+    const deck2 = deckShuffle();
+    expect(deck1).not.toEqual(deck2);
+  });
+
+  test("contains all unique cards", () => {
+    const deck = deckShuffle();
+    const uniqueCards = new Set(deck);
+    expect(uniqueCards.size).toBe(52);
+  });
+});
